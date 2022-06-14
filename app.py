1f03c8afa39caa1401baa28cdad7356b849be490
Shubh11000 <chavanshubham434@gmail.com>
2022-06-14 08:47:11 +0530

    updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Starting Machine Learning Projects"
+    return "CI CD pipe line updated"
 
 
 if __name__=="__main__":
